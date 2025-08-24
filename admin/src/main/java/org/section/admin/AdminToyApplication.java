535e86f49c830c1a8200f40aa786a6e8b66eb957
GollllDong <wjdqjatnwkd@naver.com>
2025-08-24 21:45:41 +0900

    25.08.24 admin, common 모듈 분리 및 gradle 설정

--- admin/src/main/java/org/section/admin/AdminToyApplication.java
+++ admin/src/main/java/org/section/admin/AdminToyApplication.java
@@ -1,13 +1,13 @@
-package com.toyProject.toy;
+package org.section.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ToyApplication {
+public class AdminToyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ToyApplication.class, args);
+		SpringApplication.run(AdminToyApplication.class, args);
 	}
 
 }
