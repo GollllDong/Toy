797eda58cbc01cc286c27a119f2bbd5c950d3538
beomsu00 <beomsu_jeong@tckdigital.com>
2025-09-10 22:18:28 +0900

    25.09.10 sdk 버전 및 기본 개발 환경 설정중

---
+++ admin/src/main/java/com/section/admin/content/controller/AdminNormalController.java
@@ -0,0 +1,20 @@
+package com.section.admin.content.controller;
+
+import com.section.admin.content.req.NormalListReqDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@Controller
+@RequestMapping("/content/normal")
+public class AdminNormalController {
+
+    @RequestMapping("/list")
+    public ModelAndView list(NormalListReqDto reqDto){
+        ModelAndView mav = new ModelAndView("views/normal-list");
+        mav.addObject("reqDto",reqDto);
+        return mav;
+    }
+}
