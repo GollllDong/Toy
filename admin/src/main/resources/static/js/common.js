5abc23ad3ee7061535607cb5217b8a31e55434b9
Beomballa <wjdqjatnwkd@naver.com>
2025-09-28 21:33:39 +0900

    25.09.28 홈 이동 이벤트 추가

--- admin/src/main/resources/static/js/common.js
+++ admin/src/main/resources/static/js/common.js
@@ -1,27 +1,40 @@
 // /js/common.js
+let CommonJS = {
 
-/**
- * SweetAlert2 확인 창을 띄우는 공통 함수
- * @param {string} title - 확인 창의 제목
- * @param {string} text - 확인 창의 내용
- * @param {function} onConfirm - '예' 버튼을 눌렀을 때 실행될 콜백 함수
- */
-function showConfirmAlert(title, text, onConfirm) {
-    Swal.fire({
-        title: title,
-        text: text,
-        icon: 'question',
+    init: function () {
+        document.getElementById("main-logo").addEventListener("click", function (el){
+            window.location.href = "/content/list";
+        });
+    },
 
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: '예',
-        cancelButtonText: '아니요',
+    /**
+     * SweetAlert2 확인 창을 띄우는 공통 함수
+     * @param {string} title - 확인 창의 제목
+     * @param {string} text - 확인 창의 내용
+     * @param {function} onConfirm - '예' 버튼을 눌렀을 때 실행될 콜백 함수
+     */
+    showConfirmAlert: function (title, text, onConfirm) {
+        Swal.fire({
+            title: title,
+            text: text,
+            icon: 'question',
 
-    }).then((result) => {
-        // '예' 버튼을 눌렀을 경우, 전달받은 콜백 함수를 실행
-        if (result.isConfirmed) {
-            onConfirm();
-        }
-    });
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: '예',
+            cancelButtonText: '아니요',
+
+        }).then((result) => {
+            // '예' 버튼을 눌렀을 경우, 전달받은 콜백 함수를 실행
+            if (result.isConfirmed) {
+                onConfirm();
+            }
+        });
+    }
 }
+document.addEventListener('DOMContentLoaded', function () {
+    if (typeof CommonJS !== 'undefined' && CommonJS && typeof CommonJS.init === 'function') {
+        CommonJS.init();
+    }
+});
